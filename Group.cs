41cbf1f0ad7c6504024def92f77506fd2e801ebd
hstrobbe <57355072+hstrobbe@users.noreply.github.com>
2020-01-27 15:24:43 -0600

    Add files via upload

---
+++ Group.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairedProgramming
+{
+    public class Group
+    {
+        public string Student1 { get; set; }
+        public string Student2 { get; set; }
+        public string Student3 { get; set; }
+
+        public Group(string student1,string student2,string student3) {
+            Student1 = student1;
+            Student2 = student2;
+            Student3 = student3;
+        }
+        public Group(string student1, string student2) {
+            Student1 = student1;
+            Student2 = student2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairedProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> students = new List<string>();
+            bool flag = true;
+
+            do
+            {
+                string x;
+
+
+                Console.WriteLine("Enter a Name! or press \"n\" to cancel");
+                x = Console.ReadLine();
+                if (x != "n")
+                {
+                    students.Add(x);
+                }
+                else
+                {
+                    flag = false;
+                }
+                //Console.Read(students);
+            } while (flag);
+            
+            foreach (string x in students)
+            {
+                Console.WriteLine(x);
+            }
+            List<Group> groups = new List<Group>();
+
+            if (students.Count % 2 != 0)
+            {
+                groups.Add(new Group(students[0], students[1], students[2]));
+                for (int i = 3; i < students.Count; i += 2)
+                {
+                    groups.Add(new Group(students[i], students[i + 1]));
+                }
+            }
+
+            for (int i = 0; i < students.Count; i+=2)
+            {
+                groups.Add(new Group(students[i], students[i + 1]));
+            }
+        }
+    }
+}
